9666f864646ad00403e6b9c8052f4fd112f287b0
Lukas Mahler <lukas.mah@outlook.at>
2024-10-01 21:52:38 +0200

    feat: add timeframe for rpk download in download_applogs

--- pattern_matcher/scripts/download_applogs.sh
+++ pattern_matcher/scripts/download_applogs.sh
@@ -3,6 +3,6 @@
 APPLOG_LOCATION="tmp/applogs.json"
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-kubectl exec -it -n redpanda redpanda-src-0 -- rpk topic consume applogs --offset :end -f '%v\n' > $APPLOG_LOCATION
+kubectl exec -it -n redpanda redpanda-src-0 -- rpk topic consume applogs --offset @-10m:end -f '%v\n' > $APPLOG_LOCATION
 
 "$SCRIPT_DIR"/load_local.sh $APPLOG_LOCATION
